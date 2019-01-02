05137a6bf47491b890ae72072527c69a2b6daefd
Ali Hussain <alihussein582@gmail.com>
2019-01-02 15:33:56 +0200

    process image

--- model_helper.py
+++ model_helper.py
@@ -10,6 +10,9 @@ import torch.optim as optim
 import json
 import torchvision.models as models
 import time
+from PIL import Image
+import math
+import cv2
 
 def create_dataloaders(data_dir,batch_size = 64 , mean = (0.485, 0.456, 0.406),std = (0.229, 0.224, 0.225), sampler = None):
     data_transform = transforms.Compose([    
@@ -86,3 +89,56 @@ def train_model(model, device, train_loader, valid_loader, criterion, optimizer,
         # save model if validation loss has decreased
         if valid_loss <= valid_loss_min:
             torch.save(model.state_dict(), 'model.pt')
+def process_image(image):
+    if image.size[0] > image.size[1]:
+        new_size = ( math.ceil((image.size[0]*256) / image.size[1]),256)
+    else:
+        new_size = ( 256,math.ceil((image.size[1]*256) / image.size[0]))
+    image = image.resize(new_size)
+    center_point = (math.ceil(image.size[0]/2),math.ceil(image.size[1]/2))
+    area = (center_point[0]-112,center_point[1]-112,center_point[0]+112,center_point[1]+112)
+    image = image.crop(area)
+    np_image = np.array(image)
+    np_image_normalized = cv2.normalize(np_image.astype(float),None,alpha=0,beta=1,norm_type=cv2.NORM_MINMAX)
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    np_image_normalized -= mean
+    np_image_normalized /= std
+    np_image_normalized = np_image_normalized.transpose(2,0,1)
+    return np_image_normalized
+def imshow(image, ax=None, title=None):
+    if ax is None:
+        fig, ax = plt.subplots()
+    image = image.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    image = std * image + mean
+    image = np.clip(image, 0, 1)
+    ax.imshow(image)
+    return ax
+def predict(image_path, model,device,cat_to_name,topk=5):
+    image = Image.open(image_path)
+    image = process_image(image)
+    image = torch.from_numpy(image)
+    image = image.float()
+    image = image.reshape((1,3,224,224))
+    model.eval()
+    with torch.no_grad():
+        prediction = model(image.to(device))
+    model.train()
+    prediction_softmax = F.softmax(prediction.data,dim=1)
+    top_predictions = prediction.topk(topk)
+    top_predictions_softmax = prediction_softmax.topk(topk)
+    classes = top_predictions_softmax[1][0].cpu().numpy()
+    classes_names = [cat_to_name[str(c+1)] for c in classes] 
+    prop = top_predictions_softmax[0][0].cpu().numpy()
+    
+    
+    fig, ax = plt.subplots(1,2,figsize=(20,8))
+    imshow(image[0],ax=ax[0])
+    y_pos = np.arange(topk)
+    plt.barh(y_pos, prop, align='center', alpha=0.5)
+    plt.yticks(y_pos, classes_names)
+    plt.xlabel('Probabilities')
+    plt.title('top {} probabilities'.format(topk))
+    plt.show()
